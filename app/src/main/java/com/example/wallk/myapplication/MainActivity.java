81acec829f025b6a6d97da2a2a6dafca5d15f63c
yeupanhmaj <wallkernight@gmail.com>
2018-10-24 01:32:16 +0700

    add to app center
    thêm vào app center

--- app/src/main/java/com/example/wallk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wallk/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.wallk.myapplication;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebChromeClient;
@@ -11,6 +14,7 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+
 
 public class MainActivity extends AppCompatActivity {
     WebView brow;
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "27ec27cf-7dec-4dbc-bed5-ff74c39284a4",
+                Analytics.class, Crashes.class);
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         brow= (WebView)findViewById(R.id.wv_brow);
         urledit = (EditText)findViewById(R.id.et_url);
